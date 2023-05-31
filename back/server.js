4ccfa4e7b567002aef48401805582c8005b2759f
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-05-31 19:35:25 +0900

    patch
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- back/server.js
+++ back/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+const cors = require("cors");
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 
 const productsController = require("./src/product.controller");
 const productsModels = require("./src/product.models");
@@ -8,16 +16,13 @@ app.get("/table", async (req, res) => {
   res.json(await productsController.getAll());
 });
 
-app.patch("/table", (req, res) => {
-  productsModels.update();
+app.patch("/table", async (req, res) => {
+  console.log(req.body);
+  await productsModels.update(req.body.id, req.body.comment);
+  res.json(await productsController.getAll());
 });
 
 app.listen(PORT, async () => {
-  console.log(
-    await productsModels
-      .update(3, "sold out", true)
-      .then((data) => console.log(data))
-  );
   console.log(await productsController.getAll());
   console.log(`Server listening on port ${PORT}`);
 });
